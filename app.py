5056e48f1a5e99c437465aa230935365cc5f6e4a
Eesa Zahed <eszhd1@gmail.com>
2024-08-12 15:28:01 -0400

    delete account

--- app.py
+++ app.py
@@ -12,21 +12,12 @@ db.execute("CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT)")
 
 app = Flask(__name__)
 
-def checkIfUsernameExists(username):
-    user = db.execute(f"SELECT * FROM users WHERE username='{username}'")
 
-    if user:
-        return True
-    else:
-        return False
+def checkIfUsernameExists(username):
+    return bool(db.execute(f"SELECT * FROM users WHERE username='{username}'"))
 
 def checkUsernameAndPassword(username, password):
-    user = db.execute(f"SELECT * FROM users WHERE username='{username}' AND password='{password}'")
-
-    if user:
-        return True
-    else:
-        return False
+    return bool(db.execute(f"SELECT * FROM users WHERE username='{username}' AND password='{password}'"))
 
 
 @app.route("/")
@@ -78,8 +69,6 @@ def login():
         if checkUsernameAndPassword(username, password) == False:
             return render_template("incorrectlogininfo.html") 
 
-
-        print("GENERATING JWT")
         encoded_jwt = jwt.encode({ "username": username}, secret, algorithm="HS256")
 
         return render_template("loggedin.html", username=username, jwt=encoded_jwt)
@@ -99,6 +88,26 @@ def dashboard():
             return redirect("/login")
     return redirect("/login")
 
+
+@app.route("/deleteaccount", methods=["POST"])
+def deleteaccount():
+    token = request.cookies.get("jwt")
+
+    if token:
+        try:
+            data = jwt.decode(token, secret, algorithms=["HS256"])
+            db.execute(f"DELETE FROM users WHERE username='{data['username']}'")
+
+            return "deleted"
+        except:
+            return "could not delete"
+    return "no token"
+
+@app.route("/allusernames")
+def allusernames():
+    all = db.execute("SELECT DISTINCT username FROM USERS")
+
+    return { "data": all }
 
 if __name__ == "__main__":
     app.run()
