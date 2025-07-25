93523dee18f1bc1b8b4867536f77353d98340307
Eesa Zahed <eesa.zahed@gmail.com>
2025-07-24 23:36:50 -0500

    add styles and hosting config

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ import jwt
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 secret = os.getenv("SECRET")
+
+
+if not os.path.exists("database.db"):
+    open("database.db", "w").close()
+
 
 db = SQL("sqlite:///database.db")
 db.execute("CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT)")
@@ -16,6 +22,7 @@ app = Flask(__name__)
 def checkIfUsernameExists(username):
     return bool(db.execute(f"SELECT * FROM users WHERE username='{username}'"))
 
+
 def checkUsernameAndPassword(username, password):
     return bool(db.execute(f"SELECT * FROM users WHERE username='{username}' AND password='{password}'"))
 
@@ -24,10 +31,11 @@ def checkUsernameAndPassword(username, password):
 def index():
     return render_template("index.html")
 
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     token = request.cookies.get("jwt")
-    
+
     if token:
         try:
             jwt.decode(token, secret, algorithms=["HS256"])
@@ -40,18 +48,20 @@ def register():
         password = request.form.get("password")
 
         if checkIfUsernameExists(username):
-            return render_template("unavailableusername.html", username=username) 
+            return render_template("unavailableusername.html", username=username)
 
-        db.execute("INSERT INTO users (username, password) VALUES(?, ?)", username, password)
+        db.execute(
+            "INSERT INTO users (username, password) VALUES(?, ?)", username, password)
 
         return render_template("registered.html", username=username)
 
     return render_template("register.html")
 
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     token = request.cookies.get("jwt")
-    
+
     if token:
         try:
             jwt.decode(token, secret, algorithms=["HS256"])
@@ -64,12 +74,13 @@ def login():
         password = request.form.get("password")
 
         if checkIfUsernameExists(username) == False:
-            return render_template("userdoesntexist.html", username=username) 
+            return render_template("userdoesntexist.html", username=username)
 
         if checkUsernameAndPassword(username, password) == False:
-            return render_template("incorrectlogininfo.html") 
+            return render_template("incorrectlogininfo.html")
 
-        encoded_jwt = jwt.encode({ "username": username}, secret, algorithm="HS256")
+        encoded_jwt = jwt.encode(
+            {"username": username}, secret, algorithm="HS256")
 
         return render_template("loggedin.html", username=username, jwt=encoded_jwt)
 
@@ -83,7 +94,7 @@ def dashboard():
     if token:
         try:
             data = jwt.decode(token, secret, algorithms=["HS256"])
-            return render_template("dashboard.html", username=data['username']) 
+            return render_template("dashboard.html", username=data['username'])
         except:
             return redirect("/login")
     return redirect("/login")
@@ -96,18 +107,21 @@ def deleteaccount():
     if token:
         try:
             data = jwt.decode(token, secret, algorithms=["HS256"])
-            db.execute(f"DELETE FROM users WHERE username='{data['username']}'")
+            db.execute(
+                f"DELETE FROM users WHERE username='{data['username']}'")
 
             return "deleted"
         except:
             return "could not delete"
     return "no token"
 
+
 @app.route("/allusernames")
 def allusernames():
     all = db.execute("SELECT DISTINCT username FROM USERS")
 
-    return { "data": all }
+    return {"data": all}
+
 
 if __name__ == "__main__":
     app.run()
